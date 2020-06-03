63bdcab7ffbab08120127f2b56e1fe558043ed5c
Santhiya Nagarajan <santhiya.naga@gmail.com>
2020-06-03 02:00:38 -0700

    Add bot to check cache functionality

--- zoomapi/src/main/java/com/app/bots/Bot5.java
+++ zoomapi/src/main/java/com/app/bots/Bot5.java
@@ -23,9 +23,6 @@ public class Bot5 {
 
             com.app.zoomapi.clients.OAuthClient client = credentialsHandler.getOAuthClient();
             String clientId = credentialsHandler.getClientId();
-                    /*new com.app.zoomapi.clients.OAuthClient
-                    (credentialsHandler.getClientId(), credentialsHandler.getClientSecret(), credentialsHandler.getPort(),
-                            url, credentialsHandler.getBrowserPath(), null, null,credentialsHandler.getDbPath());*/
 
             Map<String,Object> pathMap = new HashMap<>(){{put("userId","me");}};
             HttpResponse<String> response = client.getUserComponentWrapper().get(clientId,pathMap,null);
@@ -49,7 +46,6 @@ public class Bot5 {
             }
             System.out.println("-------------------------------------------------------------------------------------");
 
-
             /**
              * to create channel
              */
@@ -67,9 +63,18 @@ public class Bot5 {
             System.out.println("Adding anjanak1@uci.edu to the channel");
             response = client.getChatChannelsComponentWrapper().createChannel(clientId,channelMap);
             System.out.println(response);
-            //String channelid = JsonParser.parseString(response.body()).getAsJsonObject().get("id").getAsString();
             final String cid = JsonParser.parseString(response.body()).getAsJsonObject().get("id").getAsString();
             System.out.println("Channel created with id: "+cid);
+
+            response = client.getChatChannelsComponentWrapper().list(true,clientId);
+            channels = JsonParser.parseString(response.body()).getAsJsonObject().get("channels").getAsJsonArray();
+            System.out.println("(Immediate Cache) User's current channel list ");
+            for(JsonElement channel: channels){
+                System.out.print("Channel Name: "+channel.getAsJsonObject().get("name"));
+                System.out.println(" ID: "+channel.getAsJsonObject().get("id"));
+
+            }
+
             System.out.println("-------------------------------------------------------------------------------------");
 
             /**
@@ -98,7 +103,7 @@ public class Bot5 {
             for(JsonElement ar:array){
                 System.out.println(ar.getAsJsonObject().get("email"));
             }
-            System.out.println("-------------------------------------------------------------------------------------");
+
             System.out.println("(Immediate Cache)Getting list of members in the channel "+cid);
             pathMap = new HashMap<>(){{put("channel_id",cid);}};
             response = client.getChatChannelsComponentWrapper().listChannelMembers(true,pathMap,null);
@@ -107,6 +112,7 @@ public class Bot5 {
             for(JsonElement ar:array){
                 System.out.println(ar.getAsJsonObject().get("email"));
             }
+            System.out.println("-------------------------------------------------------------------------------------");
 
             /**
              * update channel name
@@ -122,25 +128,16 @@ public class Bot5 {
             System.out.println(response);
             System.out.println("-------------------------------------------------------------------------------------");
 
-            //test - channel cid
-            String chid = "109ab13498c64fd5911a42be1076ea6b";
-            System.out.println("Getting channel details for channel "+chid);
-            pathMap = new HashMap<>();
-            pathMap.put("channel_id",chid);
-            response = client.getChatChannelsComponentWrapper().get(false,pathMap);
-            System.out.println(response.body());
-            System.out.println("-------------------------------------------------------------------------------------");
-
             /**
              * get channel details
              */
-            /*System.out.println("Getting channel details for channel "+cid);
+            System.out.println("Getting channel details for channel "+cid);
             pathMap = new HashMap<>();
             pathMap.put("channel_id",cid);
-            response = client.getChatChannelsComponent().get(pathMap);
+            response = client.getChatChannelsComponentWrapper().get(true,pathMap);
             channelName = JsonParser.parseString(response.body()).getAsJsonObject().get("name").getAsString();
             System.out.println("Name: " + channelName);
-            System.out.println("-------------------------------------------------------------------------------------"); */
+            System.out.println("-------------------------------------------------------------------------------------");
 
             /**
              * send a chat message
@@ -171,7 +168,6 @@ public class Bot5 {
                 System.out.print(ar.getAsJsonObject().get("sender")+":");
                 System.out.println(ar.getAsJsonObject().get("message"));
             }
-            System.out.println("-------------------------------------------------------------------------------------");
 
             pathMap = new HashMap<>(){{put("userId",userId);}};
             paramMap = new HashMap<>(){{put("to_channel",cid);}};
@@ -196,8 +192,6 @@ public class Bot5 {
             dataMap.put("to_channel", cid);
             response = client.getChatMessagesComponentWrapper().update(pathMap,null,dataMap);
             System.out.println(response);
-            System.out.println("-------------------------------------------------------------------------------------");
-            Thread.sleep(3000);
 
             pathMap = new HashMap<>(){{put("userId",userId);}};
             paramMap = new HashMap<>(){{put("to_channel",cid);}};
@@ -221,13 +215,11 @@ public class Bot5 {
             response = client.getChatMessagesComponentWrapper().delete(pathMap,paramMap);
             System.out.println(response);
             System.out.println("Message deleted in Channel "+cid);
-            System.out.println("-------------------------------------------------------------------------------------");
-            Thread.sleep(3000);
 
             pathMap = new HashMap<>(){{put("userId",userId);}};
             paramMap = new HashMap<>(){{put("to_channel",cid);}};
             response = client.getChatMessagesComponentWrapper().list(true,clientId,pathMap,paramMap);
-            System.out.println("(Immediate Cache)Listing messages sent to Channel "+cid);
+            System.out.println("(Immediate Cache) Listing messages sent to Channel "+cid);
             System.out.println("For User ID: "+userId);
             array = JsonParser.parseString(response.body()).getAsJsonObject().get("messages").getAsJsonArray();
 
@@ -246,8 +238,6 @@ public class Bot5 {
             response = client.getChatChannelsComponentWrapper().remove(pathMap);
             System.out.println(response);
             System.out.println("Removed member");
-            System.out.println("-------------------------------------------------------------------------------------");
-            Thread.sleep(3000);
 
             System.out.println("(Immediate Cache) Getting list of members in the channel "+cid);
             pathMap = new HashMap<>(){{put("channel_id",cid);}};
@@ -264,23 +254,12 @@ public class Bot5 {
              */
             System.out.println("Channel id: "+cid);
             System.out.println("Leaving the channel...");
-            Thread.sleep(3000);
+            Thread.sleep(4000);
             pathMap.put("channel_id",cid);
             response = client.getChatChannelsComponentWrapper().leaveChannel(clientId,pathMap);
             System.out.println(response);
             System.out.println("Left channel with id: "+cid);
             System.out.println("The admin powers are given to the existing members!");
-            System.out.println("-------------------------------------------------------------------------------------");
-            Thread.sleep(4000);
-
-            System.out.println("(Immediate Cache) Getting list of members in the channel "+cid);
-            pathMap = new HashMap<>(){{put("channel_id",cid);}};
-            response = client.getChatChannelsComponentWrapper().listChannelMembers(true,pathMap,null);
-            System.out.println(response);
-            array = JsonParser.parseString(response.body()).getAsJsonObject().get("members").getAsJsonArray();
-            for(JsonElement ar:array){
-                System.out.println(ar.getAsJsonObject().get("email"));
-            }
 
             response = client.getChatChannelsComponentWrapper().list(true,clientId);
             channels = JsonParser.parseString(response.body()).getAsJsonObject().get("channels").getAsJsonArray();
@@ -295,16 +274,14 @@ public class Bot5 {
             /**
              * join a channel
              */
-            //TODO check functioning
+            //TODO check functioning: getting 400 always, weird.
             System.out.println("Channel id: "+cid);
             System.out.println("Joining the channel...");
-            Thread.sleep(3000);
+            Thread.sleep(6000);
             pathMap.put("channel_id",cid);
             response = client.getChatChannelsComponentWrapper().joinChannel(clientId,pathMap);
             System.out.println(response);
             System.out.println("Joined channel with id: "+cid);
-            System.out.println("-------------------------------------------------------------------------------------");
-            Thread.sleep(3000);
 
             response = client.getChatChannelsComponentWrapper().list(true,clientId);
             channels = JsonParser.parseString(response.body()).getAsJsonObject().get("channels").getAsJsonArray();
@@ -316,6 +293,58 @@ public class Bot5 {
             }
             System.out.println("-------------------------------------------------------------------------------------");
 
+            /**
+             * create a new channel to test deletion
+             * as you are not the admin for previously created after leaving the channel
+             */
+            System.out.println("You are not the admin anymore for the above channel.");
+            System.out.println("Let's create new channel to test deleting! ");
+            System.out.println("Enter channel name: ");
+            channelName = in.nextLine();
+            channelType = "1";
+            members = new ArrayList<>();
+            members.add(new HashMap<String,String>(){{put("email","anjanak1@uci.edu");}});
+            channelMap = new HashMap<>();
+            channelMap.put("name",channelName);
+            channelMap.put("type",channelType);
+            channelMap.put("members",members);
+            System.out.println("Adding anjanak1@uci.edu");
+            response = client.getChatChannelsComponentWrapper().createChannel(clientId,channelMap);
+            System.out.println(response);
+            String newCid = JsonParser.parseString(response.body()).getAsJsonObject().get("id").getAsString();
+            System.out.println("Channel created with id: "+newCid);
+
+            response = client.getChatChannelsComponentWrapper().list(true,clientId);
+            channels = JsonParser.parseString(response.body()).getAsJsonObject().get("channels").getAsJsonArray();
+            System.out.println("(Immediate Cache) User's current channel list ");
+            for(JsonElement channel: channels){
+                System.out.print("Channel Name: "+channel.getAsJsonObject().get("name"));
+                System.out.println(" ID: "+channel.getAsJsonObject().get("id"));
+
+            }
+            System.out.println("-------------------------------------------------------------------------------------");
+            Thread.sleep(3000);
+
+            /**
+             * delete a channel
+             */
+            System.out.println("Channel Id:"+newCid);
+            System.out.println("Deleting the channel...");
+            Thread.sleep(2000);
+            dataMap.put("channel_id",newCid);
+            response = client.getChatChannelsComponentWrapper().delete(dataMap);
+            System.out.println(response);
+            System.out.println("Deleted channel with id: "+newCid);
+
+            response = client.getChatChannelsComponentWrapper().list(true,clientId);
+            channels = JsonParser.parseString(response.body()).getAsJsonObject().get("channels").getAsJsonArray();
+            System.out.println("(Immediate Cache) User's current channel list ");
+            for(JsonElement channel: channels){
+                System.out.print("Channel Name: "+channel.getAsJsonObject().get("name"));
+                System.out.println(" ID: "+channel.getAsJsonObject().get("id"));
+
+            }
+            System.out.println("-------------------------------------------------------------------------------------");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
